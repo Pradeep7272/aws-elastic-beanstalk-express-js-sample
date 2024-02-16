192fc284a3ad55cc4402a6ebc2e771b7d9e55aaf
Pradeep7272 <160203460+Pradeep7272@users.noreply.github.com>
2024-02-16 15:33:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('What is new'));
+app.get('/', (req, res) => res.send('THIS IS TEAM 11 WORKING FOR DEVOPS PROJECT...........What is new'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
